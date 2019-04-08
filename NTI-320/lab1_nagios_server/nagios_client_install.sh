71209393497290954718faa7d4635a679e8839bd
chuanisawesome <cchang30@seattlecentral.edu>
2019-04-08 16:25:30 -0700

    AUTOMATED

--- NTI-320/lab1_nagios_server/nagios_client_install.sh
+++ NTI-320/lab1_nagios_server/nagios_client_install.sh
@@ -8,14 +8,17 @@
 
 yum -y install nagios-nrpe-server nagios-plugins
 
+#####NRPE INSTALLATION#####
 yum -y install nrpe
 systemctl enable nrpe
 systemctl start nrpe
 
+#####SERVER NAME#####
 nagios_server="nagios-a"
-
+#####INTERNAL IP#####
 nagios_ip=$(gcloud compute instances list | grep $nagios_server | awk '{ print $4 }' | tail -1)
 
-sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, $nagios_ip/g' /etc/nagios/nrpe.cfg
+#####ALLOW NAGIOS SERVER#####
+sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, '$nagios_ip'/g' /etc/nagios/nrpe.cfg
 
 systemctl restart nrpe
