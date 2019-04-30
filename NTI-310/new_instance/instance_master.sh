9238de6ae62d1a4881443f04894796e375b62dd9
chuanisawesome <cchang30@seattlecentral.edu>
2019-04-29 20:03:07 -0700

    dir check

--- NTI-310/new_instance/instance_master.sh
+++ NTI-310/new_instance/instance_master.sh
@@ -14,22 +14,22 @@ gcloud compute instances create $rsyslog_server \
     --scopes cloud-platform \
     --image-family centos-7 \
     --image-project centos-cloud \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/rsyslog-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/rsyslog-startup-script.sh"
 
 
 rsyslog_ip=$(gcloud compute instances list | grep $rsyslog_server | awk '{ print $4 }' | tail -1)
 echo "This is your internal rsyslog_ip $rsyslog_ip" >> instances_ip.txt
 
-ldapserver=/LinuxPojects/NTI-310/new_instance/ldap-startup-script.sh
+ldapserver=/LinuxProjects/NTI-310/new_instance/ldap-startup-script.sh
 sed -i "s/\$rsys_ip/$rsyslog_ip/g" $ldapserver
 
-nfsserver=/LinuxPojects/NTI-310/new_instance/nfs-startup-script.sh
+nfsserver=/LinuxProjects/NTI-310/new_instance/nfs-startup-script.sh
 sed -i "s/\$rsys_ip/$rsyslog_ip/g" $nfsserver
 
-postgresserver=/LinuxPojects/NTI-310/new_instance/postgres-startup-script.sh
+postgresserver=/LinuxProjects/NTI-310/new_instance/postgres-startup-script.sh
 sed -i "s/\$rsys_ip/$rsyslog_ip/g" $postgresserver
 
-djangoserver=/LinuxPojects/NTI-310/new_instance/django-startup-script.sh
+djangoserver=/LinuxProjects/NTI-310/new_instance/django-startup-script.sh
 sed -i "s/\$rsys_ip/$rsyslog_ip/g" $djangoserver
 
 sleep 5
@@ -44,7 +44,7 @@ gcloud compute instances create $ldap_server \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server","https-server" \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/ldap-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/ldap-startup-script.sh"
 
 
 ldap_ip=$(gcloud compute instances list | grep $ldap_server | awk '{ print $4 }' | tail -1)
@@ -60,13 +60,13 @@ gcloud compute instances create $nfs_server \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server","https-server" \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/nfs-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/nfs-startup-script.sh"
 
 nfs_ip=$(gcloud compute instances list | grep $nfs_server | awk '{ print $4 }' | tail -1)
 echo "This is your internal nfs_ip $nfs_ip" >> instances_ip.txt
 
 ##sed line that changes ip in the client file
-lnclient=/LinuxPojects/NTI-310/new_instance/ldap-nfs-client-startup-script.sh
+lnclient=/LinuxProjects/NTI-310/new_instance/ldap-nfs-client-startup-script.sh
 sed -i "s/\$nfs_ip/$nfs_ip/g" $lnclient
 sed -i "s/\$ldap_ip/$ldap_ip/g" $lnclient
 
@@ -81,7 +81,7 @@ gcloud compute instances create $ldap_nfs_client \
     --scopes cloud-platform \
     --image-family ubuntu-1604-lts \
     --image-project ubuntu-os-cloud \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/ldap-nfs-client-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/ldap-nfs-client-startup-script.sh"
 
 lnc_ip=$(gcloud compute instances list | grep $ldap_nfs_client | awk '{ print $4 }' | tail -1)
 echo "This is your internal clients_ip $lnc_ip" >> instances_ip.txt
@@ -97,12 +97,12 @@ gcloud compute instances create $postgres_server \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server","https-server" \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/postgres-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/postgres-startup-script.sh"
 
 post_ip=$(gcloud compute instances list | grep $postgres_server | awk '{ print $4 }' | tail -1)
 echo "This is your internal postgres_ip $post_ip" >> instances_ip.txt
 
-django=/LinuxPojects/NTI-310/new_instance/django-startup-script.sh
+django=/LinuxProjects/NTI-310/new_instance/django-startup-script.sh
 # to get postgres internal ip
 sed -i "s/\$server_name/$postgres_server/g" $django
 
@@ -118,14 +118,14 @@ gcloud compute instances create $django_server \
     --image-family centos-7 \
     --image-project centos-cloud \
     --tags "http-server","https-server","seahub-web-interface" \
-    --metadata-from-file startup-script="/LinuxPojects/NTI-310/new_instance/django-startup-script.sh"
+    --metadata-from-file startup-script="/LinuxProjects/NTI-310/new_instance/django-startup-script.sh"
 
     
 django_ip=$(gcloud compute instances list | grep $django_server | awk '{ print $4 }' | tail -1)
 echo "This is your internal django_ip $django_ip" >> instances_ip.txt
 
 
-dir=/LinuxPojects/NTI-310/new_instance/ip.sh
+dir=/LinuxProjects/NTI-310/new_instance/ip.sh
 if [ -f $dir ]
 then 
    sed -i "s/\$ldap_server/$ldap_server/g" $dir
